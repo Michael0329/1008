b5859dec586fd089794ef61b00aba86c1bc1dea9
Lucas Caldeira <lucas.it08@gmail.com>
2017-08-16 21:57:29 -0300

    Shows picture on screen in a very crude way.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,84 @@
 #include "SDL2/SDL.h"
 #include <iostream>
 
+	bool init ();
+	bool loadMedia();
+	void close ();
+
+	SDL_Window* janelaBoa = NULL;
+	SDL_Surface* surfaceMaraviliha = NULL;
+	SDL_Surface* imagem = NULL;
+
+	bool init (){
+		bool hasStarted = true;
+
+		if (SDL_INIT_VIDEO < 0){
+			std::cout << SDL_GetError() << std::endl;
+			hasStarted = false;
+		}
+
+		else {
+			janelaBoa = SDL_CreateWindow ("aeho", 100, 100, 480, 320, SDL_WINDOW_SHOWN);
+
+			if (janelaBoa == NULL){
+				std::cout << "deu ruim" << SDL_GetError() << std::endl;
+				hasStarted = false;
+			}
+
+			else {
+				surfaceMaraviliha = SDL_GetWindowSurface (janelaBoa);
+			}
+		}
+
+		return hasStarted;
+	}
+
+	bool loadMedia () {
+
+		bool hasStarted = true;
+
+		imagem = SDL_LoadBMP ("kek.bmp");
+		if (imagem == NULL){
+			std::cout << "deu ruim" << SDL_GetError();
+			hasStarted = false;
+		}
+
+		return hasStarted;
+	}
+
+	void close () {
+
+		SDL_FreeSurface (imagem);
+		imagem = NULL;
+
+		SDL_DestroyWindow (janelaBoa);
+		janelaBoa = NULL;
+
+		SDL_Quit();
+
+	}
+
+
 int main (int argc, char* args[]) {
-	
-	if (SDL_Init (SDL_INIT_VIDEO || SDL_INIT_GAMECONTROLLER) != 0 ){
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
+
+	if (!init()){
+		std::cout << "deu ruim na inicialização";
 	}
 
-	SDL_Window *win = SDL_CreateWindow ("JanelaBoa", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-while (true){
-	if (win == nullptr){
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-	}
-}
-	SDL_Quit();
+	else {
+		if (!loadMedia()){
+			std::cout << "deu ruim em loadar";
+		}
 
-	return 1;
+		else {
+			SDL_BlitSurface (imagem, NULL, surfaceMaraviliha, NULL);
+			SDL_UpdateWindowSurface (janelaBoa);
+			SDL_Delay (5000);
+		}
+	}
+
+	close();
+	return 0;
 }
+
+
